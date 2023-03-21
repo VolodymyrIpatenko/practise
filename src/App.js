bd2bb8da8e614305b49ed99c6b166e72b8e89be0
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2023-03-21 22:56:14 +0200

    observer

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css';
+import '@fremtind/jkl-core/core.min.css';
+import { MobileLink } from './MobileLink';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="spacer--small" />
+      <MobileLink>First header</MobileLink>
+      <div className="spacer--small" />
+      <MobileLink>Middle header</MobileLink>
+      <div className="spacer--small" />
+      <MobileLink>Last header</MobileLink>
+      <div className="spacer--small" />
+      <div className="spacer--small" />
+      <div className="spacer--small" />
+    </div>
+  );
+}
+
