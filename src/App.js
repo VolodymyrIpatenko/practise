5e7d860bbade98af00c1d62bf2757b54284ab8e5
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2023-04-05 12:33:17 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './style.css';
 import '@fremtind/jkl-core/core.min.css';
 import { MobileLink } from './MobileLink';
+import { Link } from 'react-router-dom';
+
+const arr = ['Hello', 'Hello', 'Hello'];
 
 export default function App() {
   return (
-    <div className="App">
-      <MobileLink>First header</MobileLink>
-    </div>
+    <section className="App">
+      <MobileLink>
+        <ul>
+          {arr.map(el => {
+            return <li>{el}</li>;
+          })}
+        </ul>
+      </MobileLink>
+    </section>
   );
 }
 
